9f9fbc62f71bb1584a023751a5993149077e217e
Kelvin Mazeika <kelvin.mzk@gmail.com>
2026-05-06 20:28:22 -0300

    Foi adicionado o código de Alerta.

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,121 @@
 #include "secrets.h"
 #include <LED.h>
 
+const uint8_t PINO_LED_RGB = 48;
+const uint8_t QUANTIDADE_LEDS = 1;
+
+Adafruit_NeoPixel ledRGB(
+    QUANTIDADE_LEDS,
+    PINO_LED_RGB,
+    NEO_GRB + NEO_KHZ800 // Duas constantes que definem a saidas do rgb e a frquencia de propagacao da informacao entre os LED`s, nesse caso é igual a 82
+);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+Led lampada(38);
+
+const char TOPICO_COMANDO[] = "wsd/comando";
+const char TOPICO_CENTRAL[] = "wsd/central";
+const char TOPICO_LATERAIS[] = "wsd/laterais";
+
+int countdown = 20;
+
+// PROTOTIPOS
+
+void tratarMensagemRecebida(const char *topico, const String &mensagem);
+void tratarComando(const String &mensagem);
+void configuraLedRGB();
+void alterarCorLedRGB(int vermelho, int verde, int azul);
+void modoAlerta();
+
 void setup() 
 {
+  configurarDebug();
 
+  configuraLedRGB();
+
+  conectarWiFi();
+  configurarDebug();
+  registrarCallbackMensagem(tratarMensagemRecebida);
+  conectarMQTT();
+
+  lcd.init();
+  lcd.backlight();
+  lcd.print("WS DEVICE");
+
+  alterarCorLedRGB(0, 255, 0);
 }
 
 void loop()
 {
+    static unsigned long before = 0;
+    if(millis() - before >= 1000)
+    {
+        countdown--;
+        lcd.setCursor(0, 2);
+        lcd.printf("Tempo restante: %d  ", countdown);
+    }
 
+    if(countdown == 0)
+    {
+        lampada.blink(2);
+        publicarMensagem(TOPICO_LATERAIS, "alerta");
+        alterarCorLedRGB(255, 0, 0);
+    }
+
+    garantirMQTTConectado();
+    garantirWiFiConectado();
+    loopMQTT();
+}
+
+void tratarMensagemRecebida(const char *topico, const String &mensagem)
+{
+  debugInfo("==============================");
+  debugInfo("Mensagem recebida na aplicação");
+  debugInfo("==============================");
+
+  if (topico == nullptr)
+  {
+    debugErro("Tópico MQTT inválido");
+    return;
+  }
+
+  debugInfo("Tópico: " + String(topico));
+  debugInfo("Mensagem: " + mensagem);
+
+  if (strcmp(topico, TOPICO_COMANDO) == 0)
+  {
+    tratarComando(mensagem);
+    return;
+  }
+
+  if (strcmp(topico, TOPICO_CENTRAL) == 0)
+  {
+    if(strcmp(mensagem.c_str(), "pressionado"))
+    countdown = 20;
+    return;
+  }
+
+  debugErro("Tópico não tratado: " + String(topico));
+}
+
+void configuraLedRGB()
+{
+  ledRGB.begin();
+  ledRGB.setBrightness(80); // Colocamos a qtd de brilho para o led de 0 a 255
+  ledRGB.clear();
+  ledRGB.show(); // atualiza estado led
+}
+
+void alterarCorLedRGB(int vermelho, int verde, int azul)
+{
+  vermelho = constrain(vermelho, 0, 255);
+  verde = constrain(verde, 0, 255);
+  azul = constrain(azul, 0, 255);
+
+  ledRGB.setPixelColor(0, ledRGB.Color(vermelho, verde, azul));
+  ledRGB.show();
+
+  debugInfo("Cor aplicada o LED RGB");
+  debugInfo("R: " + String(vermelho));
+  debugInfo("G: " + String(verde));
+  debugInfo("B: " + String(azul));
 }
