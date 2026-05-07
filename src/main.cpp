cba9dbecf10ea39a04d5ae97f16c42cec1e292d4
MURILO DE SOUSA FERREIRA <42820593895@info.local>
2026-05-07 16:17:13 -0300

    adicionado tratamento ESP

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,41 @@
 #include "MqttManager.h"
 #include "DebugManager.h"
 #include "secrets.h"
+#include <Bounce2.h>
+
+const uint8_t QUANTIDADE_LEDS = 3;
+const uint8_t PINO_LED_RGB = 36;
+
+Adafruit_NeoPixel ledRGB(
+    QUANTIDADE_LEDS,
+    PINO_LED_RGB,
+    NEO_GRB + NEO_KHZ800); 
+
+
+Bounce botaoBoot = Bounce();
+
+void configuraLedRGB();
+void alterarCorLedRGB(int verde, int laranja, int vermelho);
 
 void setup() 
 {
-
+botaoBoot.attach(0, INPUT_PULLUP);
 }
 
 void loop()
 {
+botaoBoot.update();
+
 
 }
+
+void configuraLedRGB()
+{
+ ledRGB.begin();
+ ledRGB.setBrightness(100);
+ ledRGB.clear();
+ ledRGB.show();
+}
+
+void alterarCorLedRGB()
+{}
